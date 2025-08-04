5b0b18f61de8fdc4a2c1caa0675181f3d88c90d6
NCJ <me@ncj.wiki>
2025-08-04 20:56:41 +0800

    set default configs to base-200m

--- renderformer/models/config.py
+++ renderformer/models/config.py
@@ -4,46 +4,46 @@ from typing import Literal, List, Optional
 
 @dataclass(frozen=True)
 class RenderFormerConfig:
-    latent_dim: int = 384
+    latent_dim: int = 768
     """The latent dimension of the transformer."""
-    num_layers: int = 16
+    num_layers: int = 12
     """The number of layers in the transformer."""
-    num_heads: int = 8
+    num_heads: int = 6
     """The number of heads in the transformer."""
-    dim_feedforward: int = 384 * 4
+    dim_feedforward: int = 768 * 4
     """The number of frequencies in the positional encoding for vertex positions."""
     num_register_tokens: int = 16
     """The dimension of the feedforward network in the transformer."""
-    dropout: float = 0.1
+    dropout: float = 0.0
     """The dropout rate in the transformer."""
-    activation: Literal['gelu', 'swiglu'] = 'gelu'
+    activation: Literal['gelu', 'swiglu'] = 'swiglu'
     """The activation function in the transformer."""
-    norm_type: Literal['layer_norm', 'rms_norm'] = 'layer_norm'
+    norm_type: Literal['layer_norm', 'rms_norm'] = 'rms_norm'
     """The type of normalization to use in the transformer."""
-    norm_first: bool = False
+    norm_first: bool = True
     """Whether to normalize the input before the transformer."""
-    view_indep_qk_norm: bool = False
+    view_indep_qk_norm: bool = True
     """Whether to apply normalization to query and key."""
-    qk_norm: bool = False
+    qk_norm: bool = True
     """Whether to apply normalization to query and key."""
     bias: bool = False
     """Whether to use bias in the transformer."""
 
-    pe_type: Literal['nerf', 'rope'] = 'nerf'
+    pe_type: Literal['nerf', 'rope'] = 'rope'
     """The type of positional encoding to use."""
     rope_type: Literal['triangle', 'triangle_learned', 'triangle_mixed'] = 'triangle'
     """The type of RoPE to use."""
     rope_double_max_freq: bool = False
     """Whether to double the max frequency for RoPE."""
-    vertex_pe_num_freqs: int = 6
+    vertex_pe_num_freqs: int = 12
     """The number of frequencies in the positional encoding for vertex positions."""
 
     # vertex normal encoder
-    use_vn_encoder: bool = False
+    use_vn_encoder: bool = True
     """Whether to use the vertex normal encoder."""
     vn_pe_num_freqs: int = 6
     """The number of frequencies in the positional encoding for vertex normals."""
-    vn_encoder_norm_type: Literal['none', 'layer_norm', 'rms_norm'] = 'none'
+    vn_encoder_norm_type: Literal['none', 'layer_norm', 'rms_norm'] = 'rms_norm'
     """The type of normalization to use in the vertex normal encoder."""
 
     # texture patch encoder
@@ -51,17 +51,17 @@ class RenderFormerConfig:
     """The size of the triangle's transformed texture patch for encoder."""
     texture_channels: int = 13  # diffuse, specular, normal, roughness, irradiance
     """The number of channels in the texture patch."""
-    texture_encoder_norm_type: Literal['layer_norm', 'rms_norm'] = 'layer_norm'
+    texture_encoder_norm_type: Literal['layer_norm', 'rms_norm'] = 'rms_norm'
     """The type of normalization to use in the texture encoder."""
 
     # view transformer
-    view_transformer_latent_dim: int = 384
+    view_transformer_latent_dim: int = 768
     """The latent dimension of the view transformer."""
-    view_transformer_ffn_hidden_dim: int = 384 * 4
+    view_transformer_ffn_hidden_dim: int = 768 * 4
     """The hidden dimension of the feedforward network in the view transformer."""
-    view_transformer_n_heads: int = 8
+    view_transformer_n_heads: int = 6
     """The number of heads in the view transformer."""
-    view_transformer_n_layers: int = 10
+    view_transformer_n_layers: int = 6
     """The number of layers in the view transformer."""
     view_transformer_include_self_attn: bool = True
     """Whether to include self-attention between ray tokens in the view transformer."""
@@ -75,7 +75,7 @@ class RenderFormerConfig:
     """The size of the image patch in the view transformer."""
     include_alpha: bool = False
     """Whether to include the alpha channel in the texture patch."""
-    use_dpt_decoder: bool = False
+    use_dpt_decoder: bool = True
     """Whether to use DPT decoder for rendering."""
     dpt_features: int = 128
     """The dim of internal features in the DPT decoder."""
